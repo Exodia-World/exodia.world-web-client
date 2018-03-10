f02d8234613d0d451bf0014506877d8d0eef9e79
Victor Huberta <victorhuberta@gmail.com>
2018-03-10 22:27:33 +0700

    Add contracts service for all contract interaction purposes. It might be split into many contract-specific services, however. It seems like we need to set default account to avoid 'invalid address' error thrown by sendEther().

--- src/app/services/contracts.service.ts
+++ src/app/services/contracts.service.ts
@@ -5,22 +5,23 @@ import { MetamaskService } from './metamask.service';
 declare let require: any;
 declare let window: any;
 
-// let tokenAbi = require('./tokenContract.json');
+// const tokenAbi = require('./tokenContract.json');
+const env: any = require('../../../env.json');
 
 @Injectable()
 export class ContractsService {
   private web3: any;
 
-  constructor(metamaskService: MetamaskService) {
+  constructor(private metamaskService: MetamaskService) {
     // Setup Web3 provider.
     if (typeof window.web3 !== 'undefined') {
       this.web3 = new Web3(window.web3.currentProvider);
     } else {
-      this.web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:8545'));
+      this.web3 = new Web3(new Web3.providers.HttpProvider(env.TEST_RPC_PROVIDER));
     }
   }
 
-  sendEther(to: string, value: number) {
+  sendEther(to: string, value: number): void {
     this.web3.eth.sendTransaction({to, value}, (err, res) => {
       if (err) this.metamaskService.closeNotification();
       if (res) this.metamaskService.closeNotification();
