154338a1c4a6f90ab7c8b367b932a63095eae214
Victor Huberta <victorhuberta@gmail.com>
2018-05-25 16:55:57 +0700

    Add divider and form field.

--- src/app/wallet/wallet.module.ts
+++ src/app/wallet/wallet.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../modules/shared.module';
+
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
 import { QRCodeModule } from 'angularx-qrcode';
 import { MessageModule } from '../components/message/message.module';
 import { LongNumberPipeModule } from '../pipes/long-number/long-number.module';
@@ -17,6 +22,9 @@ import { WalletComponent } from './wallet.component';
     SharedModule,
     MatSidenavModule,
     MatTabsModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatInputModule,
     QRCodeModule,
     MessageModule,
     LongNumberPipeModule,
