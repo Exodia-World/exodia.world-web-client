5a6bd11563d6a42e051adf2a5fa95bb882572183
Victor Huberta <victorhuberta@gmail.com>
2018-06-22 20:46:43 +0700

    Use NO_ERRORS_SCHEMA and add 2 tests for balance component.

--- src/app/wallet/balance/balance.component.spec.ts
+++ src/app/wallet/balance/balance.component.spec.ts
@@ -1,11 +1,9 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { FormBuilder } from '@angular/forms';
 import { BalanceComponent } from './balance.component';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SharedModule } from '../../modules/shared.module';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 import { LongNumberPipeModule } from '../../pipes/long-number/long-number.module';
-import { MessageModule } from '../../components/message/message.module';
 
 import { Web3Service } from '../../services/web3.service';
 import { WalletService } from '../shared/wallet.service';
@@ -14,33 +12,36 @@ import { spyOnWalletService, spyOnWeb3Service, spyOnEXOToken } from '../../globa
 describe('Component: Balance', () => {
   let component: BalanceComponent;
   let fixture: ComponentFixture<BalanceComponent>;
+  let el: any;
   let WalletServiceSpy: any;
   let Web3ServiceSpy: any;
 
-  beforeEach(() => {
+  beforeEach(async(() => {
     WalletServiceSpy = spyOnWalletService();
     Web3ServiceSpy = spyOnWeb3Service(spyOnEXOToken());
 
     TestBed.configureTestingModule({
       imports: [
-        SharedModule,
-        MatFormFieldModule,
-        MatInputModule,
-        LongNumberPipeModule,
-        MessageModule
+        LongNumberPipeModule
       ],
       providers: [
         {provide: WalletService, useValue: WalletServiceSpy},
-        {provide: Web3Service, useValue: Web3ServiceSpy}
+        {provide: Web3Service, useValue: Web3ServiceSpy},
+        FormBuilder
       ],
-      declarations: [BalanceComponent]
-    });
+      declarations: [BalanceComponent],
+      schemas: [NO_ERRORS_SCHEMA]
+    }).compileComponents();
+  }));
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(BalanceComponent);
+    el = fixture.debugElement.nativeElement;
     component = fixture.componentInstance;
     spyOn(component, 'refreshAll');
 
     component.ngOnInit();
+    fixture.detectChanges();
   });
 
   it('should create component', () => {
@@ -52,11 +53,37 @@ describe('Component: Balance', () => {
     expect(component.refreshAll).toHaveBeenCalled();
   });
 
-  it('should reject negative sent amount', () => {
-    component.form.get('sentAmount').setValue(-1);
+  it('should only show form if it is maximized', () => {
+    let form = el.querySelector('form');
+    expect(form).toEqual(null);
 
-    const sendTokensBtn = fixture.nativeElement.querySelector('div');
-    console.log(sendTokensBtn);
+    component.isMaximized = true;
+    fixture.detectChanges();
+    form = el.querySelector('form');
+    expect(form).not.toEqual(null);
+  });
+
+  it('should reject sent amount that is <= 0', () => {
+    component.isMaximized = true;
+    component.form.get('sentAmount').setValue(0);
+    fixture.detectChanges();
+
+    const sendTokensBtn = el.querySelector('.send-tokens');
+    expect(component.form.invalid).toBe(true);
+    expect(sendTokensBtn.disabled).toBe(true);
+
+    component.form.get('sentAmount').setValue(-1);
+    fixture.detectChanges();
+    expect(component.form.invalid).toBe(true);
+    expect(sendTokensBtn.disabled).toBe(true);
+  });
+
+  it('should reject empty destination address', () => {
+    component.isMaximized = true;
+    component.form.get('destAddress').setValue('');
+    fixture.detectChanges();
+
+    const sendTokensBtn = el.querySelector('.send-tokens');
     expect(component.form.invalid).toBe(true);
     expect(sendTokensBtn.disabled).toBe(true);
   });
