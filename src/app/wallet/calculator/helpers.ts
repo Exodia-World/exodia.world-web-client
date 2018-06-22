ab1bc6d644a021fc92840c5ac559de07d2a1b2ee
Victor Huberta <victorhuberta@gmail.com>
2018-06-22 21:00:00 +0700

    Use ISO format for all moment date creation.

--- src/app/wallet/calculator/helpers.ts
+++ src/app/wallet/calculator/helpers.ts
@@ -1,7 +1,8 @@
 import * as moment from 'moment';
 
-export const STAKING_STARTDATE = moment('2018-7-30');
-export const FIRST_INTEREST_PERIOD_END_DATE = moment('2018-7-30').add(3, 'years');
+const startDate = (new Date('2018-7-30')).toISOString();
+export const STAKING_STARTDATE = moment(startDate);
+export const FIRST_INTEREST_PERIOD_END_DATE = moment(startDate).add(3, 'years');
 export const FIRST_INTEREST_SECOND_INTEREST_PERIOD_BETWEEN_DATE = '2020-11-15';
 export const SECOND_INTEREST_PERIOD_BETWEEN_DATE = '2022-9-15';
 export const FIRST_INTEREST_PERIOD_BETWEEN_DATE = '2018-9-15';
