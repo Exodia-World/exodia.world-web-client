a12dafc3d7d4cfaddbbdfce2c5eae9a4390bf50c
Victor Huberta <victorhuberta@gmail.com>
2018-07-19 22:08:54 +0700

    Document storage service.

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -9,11 +9,23 @@ export class StorageService {
 
   constructor(private web3Service: Web3Service) { }
 
+  /**
+   * Retrieve an item from session storage.
+   *
+   * @param {string} name Item's name to look for
+   * @returns Item's content
+   */
   getSessionItem(name: string): any {
     const key = this.prepareKey(name);
     return this.extractContent(sessionStorage.getItem(key));
   }
 
+  /**
+   * Save an item to session storage.
+   *
+   * @param {string} name Item's name to look for on retrieval
+   * @param {any} content Item's content
+   */
   setSessionItem(name: string, content: any) {
     const key = this.prepareKey(name);
     const value = this.prepareValue(content);
